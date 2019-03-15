599eb0691d11e3e10efca1c447a70283a5dad10b
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-03-15 13:21:16 +0000

    add repl info to led.js

--- led.js
+++ led.js
@@ -4,9 +4,15 @@ var board = new five.Board();
 board.on('ready', function(){
   var led = five.Led(13);
 
+// grant access to led instance from repl that is created when this is run
   this.repl.inject({
     led: led
   });
 
   led.blink();
 })
+
+// Commands from REPL:
+// led.stop()
+// led.off()
+// led.on() 
