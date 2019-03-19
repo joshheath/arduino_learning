96dc17151703818cb6a70f9afd9781e81e8da70a
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-03-19 11:51:12 +0000

    add notes to led_pulse

--- led_pulse.js
+++ led_pulse.js
@@ -5,7 +5,11 @@ board.on('ready', function(){
   var led = new five.Led(11);
   led.pulse(11);
 
+  // LED pulse loop will stop after 10s
   this.wait(10000, function(){
+
+    // stop - stops the interval
+    // off - shuts off LED
     led.stop().off();
   });
 });
