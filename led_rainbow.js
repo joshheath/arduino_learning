e581b9cef925ef6a4129547539e2071fb41d9888
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-04-15 12:25:28 +0100

    add info on hex colours

--- led_rainbow.js
+++ led_rainbow.js
@@ -4,6 +4,8 @@ var board = new five.Board();
 board.on('ready', function(){
   var rgb = new five.Led.RGB([6,5,3]);
   var index = 0;
+
+  // added a random set of hex colours for variety, found at https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
   var rainbow = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000']
 
   this.loop(200, function(){
