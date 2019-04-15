7f975242ce888fa4ce9b918f31103178ff85fe80
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-04-15 13:20:50 +0100

    add led functionality in port 13

--- button.js
+++ button.js
@@ -1,23 +1,35 @@
 var five = require('johnny-five'),
-  board, button;
+  board, button, led;
 var board = new five.Board();
 
 board.on('ready', function(){
-  button = new five.Button(2);
 
+  // creates a new button instance on port 2 - the button module can create a completely default instance
+  button = new five.Button(2);
+  var led = new five.Led(13);
+
+  // allows control from the command line by injecting the button hardware into the repl
   board.repl.inject({
     button: button
   });
 
+  // button event API
+
+  // down = button pressed
   button.on("down", function(){
     console.log('down');
+    led.on();
   });
 
+  // hold = button pressed for a specific time, 1/2 second by default
   button.on('hold', function(){
     console.log('hold');
+    led.on();
   });
 
+  // up = button released
   button.on('up', function(){
     console.log('up');
+    led.off();
   });
 });
